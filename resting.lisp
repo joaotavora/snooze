5f6ee8d29b7867c8cd73312c809f46846a45ad68
João Távora <joaotavora@gmail.com>
2014-08-31 00:52:37 +0100

    Should start testing...

--- resting.lisp
+++ resting.lisp
@@ -1,14 +1,54 @@
 (in-package #:resting)
 
-(defpackage :resting-recv-types (:use) (:export #:anything))
-(defpackage :resting-send-types (:use) (:export #:anything))
-(defpackage :resting-verbs (:use) (:export #:get #:post #:put #:delete))
 
-(defclass resting-recv-types:anything () ())
+;;; Verbs
+;;;
+;;; "Sending" and "Receiving" are always from the server's
+;;; perspective. Hence GET is "sending" and POST and PUT are
+;;; "receiving".
+;;; 
+(defpackage :resting-verbs (:use) (:export #:http-verb #:get #:post #:put #:delete
+                                           #:receiving-verb
+                                           #:sending-verb))
+(in-package :resting-verbs)
+
+(cl:defclass http-verb () ())
+
+(cl:defclass delete (http-verb) ())
+
+(cl:defclass receiving-verb (http-verb) ())
+(cl:defclass sending-verb   (http-verb) ())
+
+(cl:defclass post (receiving-verb) ())
+(cl:defclass put  (receiving-verb) ())
+
+(cl:defclass get (sending-verb) ())
+
+;;; Content-types
+;;;
+;;; Apropos the distinction between "receive" and "send" types, note
+;;; that in GET requests we are only interested in the request's
+;;; "Accept" header, since GET never have useful bodies (1) and as
+;;; such don't have "Content-Type".
+;;;
+;;; [1]: http://stackoverflow.com/questions/978061/http-get-with-request-body
+;;;
+;;; So, for GET requests, we make a inverted hierarchy of types for
+;;; dispatching on the various types of the "Accept:" header.
+;;;
+(cl:in-package :resting)
+
+(defpackage :resting-recv-types (:use) (:export #:anything))
+
+(defpackage :resting-send-types (:use) (:export #:anything))
+
+(defclass resting-recv-types:anything ()
+  ((content-stream :initarg :content-stream)))
+
 (defclass resting-send-types:anything () ())
 
 (defun intern-safe (designator package)
-  (intern (string-upcase (string designator)) package))
+  (intern (string-upcase designator) package))
 
 (defmacro define-recv-type (type-designator supertype-designator)
   (let ((type (intern-safe type-designator :resting-recv-types))
@@ -46,114 +86,199 @@
                collect `(define-recv-type ,type ,supertype)
                and collect `(define-send-type ,type ,supertype))))
 
+
+;;; Define some things at compile-time
+;;;
 (eval-when (:compile-toplevel :load-toplevel :execute)
   (define-known-content-types))
 
-(defun content-type-or-lose (designator package)
-  (let ((sym
-          (find-symbol (string-upcase (string designator))
-                       package)))
-    (unless (find-class sym nil)
-      (error "There is no HTTP Content-Type of the ~a kind designated by ~a"
-             package designator))
+
+;;; DEFROUTE Helpers
+;;; 
+(defun find-class-1 (sym)
+  "Like CL:FIND-CLASS but don't error and return SYM or nil"
+  (when (find-class sym nil)
     sym))
 
-(defun send-type-or-lose (designator)
-  (content-type-or-lose designator :resting-send-types))
+(defun verb-spec-or-lose (verb-spec)
+  "Convert VERB-SPEC into something `defmethod' can grok."
+  (labels ((verb-designator-to-verb (designator)
+             (or (and (eq designator 't)
+                      (progn
+                        (alexandria:simple-style-warning
+                         "Converting T in ~a to ~a"
+                              verb-spec 'resting-verbs:http-verb)
+                        'resting-verbs:http-verb))
+                 (find-class-1 (intern (string-upcase designator)
+                                       :resting-verbs))
+                 (error "Sorry, don't know the HTTP verb ~a"
+                        (string-upcase designator)))))
+    (cond ((and verb-spec
+                (listp verb-spec))
+           (list (first verb-spec) (verb-designator-to-verb (second verb-spec))))
+          ((or (keywordp verb-spec)
+               (stringp verb-spec))
+           (list 'verb (verb-designator-to-verb verb-spec)))
+          (verb-spec
+           (list verb-spec 'resting-verbs:http-verb)))))
 
-(defun recv-type-or-lose (designator)
-  (content-type-or-lose designator :resting-recv-types))
+(defun catch-all-type (package)
+  (intern "ANYTHING" package))
+
+(defun type-designator-to-type-1 (designator package)
+  "Return DESIGNATOR as class-defining symbol in PACKAGE, or nil."
+  (or (and (eq designator t)
+           (progn
+             (alexandria:simple-style-warning
+              "Converting content-type designator T to ~a"
+              (catch-all-type package))
+             (catch-all-type package)))
+      (find-class-1 (intern (string-upcase designator) package))
+      (and (string= designator "*/*")
+           (catch-all-type package))
+      (let* ((matches (nth-value 1
+                                 (cl-ppcre:scan-to-strings
+                                  "([^/]+)/\\*"
+                                  designator)))
+             (supertype-designator (and matches
+                                        (aref matches 0))))
+        (find-class-1 (intern (string-upcase supertype-designator)
+                              package)))))
+
+(defun content-type-spec-or-lose (type-spec verb)
+  (labels ((type-designator-to-type (designator)
+             (or 
+              (cond ((subtypep verb 'resting-verbs:receiving-verb)
+                     (type-designator-to-type-1 designator :resting-recv-types))
+                    ((subtypep verb 'resting-verbs:sending-verb) 
+                     (type-designator-to-type-1 designator :resting-send-types))
+                    (designator
+                     ;; In this case, bitch if the designator is not T
+                     ;;
+                     (assert (eq t designator) nil
+                             "For verb ~a, no specializations on Content-Type are allowed"
+                             verb)
+                     designator))
+              (error "Sorry, don't know the content-type ~a" type-spec))))
+    (cond ((and type-spec
+                (listp type-spec))
+           (list (first type-spec) (type-designator-to-type (second type-spec))))
+          ((or (keywordp type-spec)
+               (stringp type-spec))
+           (list 'type (type-designator-to-type type-spec)))
+          (type-spec
+           (list type-spec (type-designator-to-type t))))))
+
+;; half-assed tests for the DEFROUTE helpers
+;;
+;; (verb-spec-or-lose '(foo t)) ; (FOO RESTING-VERBS:HTTP-VERB)
+;; (verb-spec-or-lose :get)     ; (VERB RESTING-VERBS:GET)
+;; (verb-spec-or-lose "GET")    ; (VERB RESTING-VERBS:GET)
+;; (verb-spec-or-lose '(v resting-verbs:get)) ;(V RESTING-VERBS:GET)
+
+;; (content-type-spec-or-lose '(foo t) 'resting-verbs:get)     ; (FOO RESTING-SEND-TYPES:ANYTHING)
+;; (content-type-spec-or-lose '(foo "*/*") 'resting-verbs:get) ; (FOO RESTING-SEND-TYPES:ANYTHING)
+;; (content-type-spec-or-lose 'foo 'resting-verbs:get)         ; (FOO RESTING-SEND-TYPES:ANYTHING)
+;;  
+;; (content-type-spec-or-lose '(foo t) 'resting-verbs:put)     ; (FOO RESTING-RECV-TYPES:ANYTHING)
+;; (content-type-spec-or-lose '(foo "*/*") 'resting-verbs:put) ; (FOO RESTING-RECV-TYPES:ANYTHING)
+;; (content-type-spec-or-lose '(foo "text/*") 'resting-verbs:put) ; (FOO RESTING-RECV-TYPES:TEXT)
+;; (content-type-spec-or-lose '(foo "text/html") 'resting-verbs:put) ; (FOO RESTING-RECV-TYPES:TEXT/HTML)
+;; (content-type-spec-or-lose "text/html" 'resting-verbs:put) ; (TYPE RESTING-RECV-TYPES:TEXT/HTML)
+;; (content-type-spec-or-lose '"text/html" 'resting-verbs:put) ; (TYPE RESTING-RECV-TYPES:TEXT/HTML)
+;; (content-type-spec-or-lose '"text/html-typo" 'resting-verbs:put) ; error!
+;; (content-type-spec-or-lose 'foo 'resting-verbs:put); (FOO RESTING-RECV-TYPES:ANYTHING)
+;; (content-type-spec-or-lose '(foo resting-recv-types:text/html) 'resting-verbs:put) ; (FOO RESTING-RECV-TYPES:TEXT/HTML)
+;; (content-type-spec-or-lose '(foo resting-recv-types:text-html-typo) 'resting-verbs:put) ; error!
+;; (content-type-spec-or-lose '(foo resting-send-types:text/html) 'resting-verbs:put); (FOO RESTING-RECV-TYPES:TEXT/HTML) ! autocorrect!
+
+;; (content-type-spec-or-lose 'foo 'resting-verbs:http-verb); (FOO T)
+;; (content-type-spec-or-lose "text/html" 'resting-verbs:http-verb) ; error!
+;; (content-type-spec-or-lose "text/html" 'resting-verbs:delete) ;error!
 
 
 
-(defun http-verb-or-lose (thing)
-  (let ((sym
-          (find-symbol (string-upcase (string thing))
-                       :resting-verbs)))
-    (or sym
-        (error "No such \"~a\" HTTP verb" thing))))
+
+;; DEFROUTE macro
+;; 
+(define-condition no-such-route (simple-error) ())
+
+(defmacro defroute (name-or-name-and-qualifier (verb-spec type-spec &rest args) &body body)
+  (let* ((name-and-qualifier (alexandria:ensure-list name-or-name-and-qualifier))
+         (name (first name-and-qualifier))
+         (qualifier (second name-and-qualifier))
+         (verb-spec (verb-spec-or-lose verb-spec))
+         (type-spec (content-type-spec-or-lose type-spec (second verb-spec))))
+    `((defmethod no-applicable-method ((f (eql (function ,name))) &rest ignored)
+        (declare (ignore ignored))
+        (error 'no-such-route))
+      (defmethod ,name ,@(and (keywordp qualifier) (list qualifier))
+        (,verb-spec ,type-spec ,@args)
+        ,@body)
+      (setf (get ,name 'resting::route) t))))
+
+;; (defroute picture (:GET "image/jpeg" user-id album-id) bla)
+;; (defroute (picture :before) (:GET "image/jpeg" user-id album-id) bla)
+;; (defroute :around (verb type user-id album-id) bla)
+
+
 
 (defclass rest-acceptor (hunchentoot:acceptor)
-  ()
+  ((route-packages :initform (list *package*) :initarg :route-packages :accessor route-packages))
   (:documentation "An acceptor for RESTful routes"))
 
 
 (defparameter *default-send-type* 'resting-send-types:text/html)
 (defparameter *default-recv-type* 'resting-recv-types:anything)
 
-(define-condition no-such-route (simple-error) ())
 
-(defmacro defroute (name-or-name-and-options args &body body)
-  (let* ((name-and-options (alexandria:ensure-list name-or-name-and-options))
-         (name (car name-and-options))
-         (options (cdr name-and-options))
-         (verb-designator (getf options :verb))
-         (verb (or (and verb-designator
-                        (http-verb-or-lose verb-designator))
-                   'resting-verbs:get))
-         (content-designator (getf options :content-type))
-         (type
-           (ecase verb
-             ;; For the Accept: header
-             (resting-verbs:get (or (and content-designator
-                                         (content-type-or-lose content-designator :resting-send-types))
-                                    *default-send-type*))
-             ;; For the Content-Type: header
-             ((resting-verbs:put
-               resting-verbs:post)
-              (or (and content-designator
-                       (content-type-or-lose content-designator :resting-recv-types))
-                  *default-recv-type*))
-             (resting-verbs:delete
-              ;; There can be no content in either direction.
-              (assert (not content-designator) nil
-                      "For HTTP DELETE, Content-Type doesn't make sense")))))
-    `((defmethod no-applicable-method ((f (eql (function ,name))) &rest ignored)
-        (declare (ignore ignored))
-        (error 'no-such-route))
-      (defmethod ,name ((resting::verb ,verb)
-                        (resting::type ,type)
-                        ,@args)
-        ,@body))))
 
+
+
+;;; Helpers for our HUNCHENTOOT:ACCEPTOR-DISPATCH-REQUEST method
+;;;
+;;; FIXME: very naive, need lots of work
 (defun parse-uri (uri)
-  ;; FIXME needs lots of work
   (cl-ppcre:split "/" uri :start 1))
 
+(defun parse-accept-header (string)
+  "Return a list of symbols designating RESTING-RECV-TYPE objects.
+In the correct order" 
+  (cl-ppcre:split "," string))
+
+(defun parse-content-type-header (string)
+  "Return a symbol designating a RESTING-SEND-TYPE object."
+  (type-designator-to-type-1 string :resting-recv-types))
 
 (defmethod hunchentoot:acceptor-dispatch-request ((acceptor rest-acceptor) request)
-  (let* ((accept (hunchentoot:header-in :accept request))
-         (accepted-types (or (and accept (cl-ppcre:split "," accept))
-                             'resting-send-types:anything))
-         (content-type (hunchentoot:header-in :content-type request))
-         (verb-designator (hunchentoot:request-method request))
-         (verb (http-verb-or-lose verb-designator))
-         (uri (hunchentoot:script-name request))
-         ;; FIXME needs a lot of work
-         (uri-and-args (parse-uri uri)))
-    (when (and (eq 'resting-verbs:get verb)
-               content-type)
-      (error "Sorry, no \"Content-Type:\" allowed for HTTP GET"))
-    (when (and (member verb '(resting-verbs:post resting-verbs:put))
-               accepted-types)
-      (error "Sorry, no \"Accept:\" header allowed for HTTP GET"))
-    (ecase verb
+  (let* ((accept-designators (parse-accept-header (hunchentoot:header-in :accept request)))
+         (content-type (parse-content-type-header (hunchentoot:header-in :content-type request)))
+         (verb (find-class (intern (string-upcase (hunchentoot:request-method request)))))
+         (uri-and-args (parse-uri (hunchentoot:script-name request)))
+         (route-method (loop for package in (route-packages acceptor)
+                               thereis (find-symbol (car uri-and-args) package))))
+    (etypecase verb
       ;; For the Accept: header
-      (resting-verbs:get
-       (when content-type
-         (error "Sorry, no \"Content-Type:\" allowed for HTTP GET"))
-       (loop for type in (mapcar #'send-type-or-lose accepted-types)
-             do (apply (car uri-and-args)
-                       (make-instance verb)
-                       (make-instance type)
-                       (cdr uri-and-args)))
-      ((resting-verbs:put
-        resting-verbs:post
-        resting-verbs:delete)
-       (apply (car uri-and-args)
+      (resting-verbs:sending-verb
+       (loop for (type . more) on accept-designators
+             do
+                (block try-again
+                  (handler-bind ((no-such-route
+                                   #'(lambda (c)
+                                       (declare (ignore c))
+                                       (if more
+                                           (return-from try-again)))))
+                    (apply route-method
+                           (make-instance verb)
+                           (make-instance type)
+                           (cdr uri-and-args))))))
+      (resting-verbs:receiving-verb
+       (apply route-method
               (make-instance verb)
-              (make-instance (recv-type-or-lose content-type))
-              (cdr uri-and-args)))))))
+              (make-instance (or content-type
+                                 'resting-recv-types:anything)
+                             :content-stream 'something)
+              (cdr uri-and-args))))))
 
 (defmethod hunchentoot:acceptor-status-message ((acceptor rest-acceptor) status-code &key
                                                 &allow-other-keys)
